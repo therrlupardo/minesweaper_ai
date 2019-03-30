ce132edb6e6612f17dbb6b7b26018d15dedb20ee
Mateusz Buchajewicz <theherrlupardo@gmail.com>
2019-03-30 15:47:53 +0100

    Simple method solving version 1

--- Field.py
+++ Field.py
@@ -1,27 +1,29 @@
 class Field:
-    __is_mine = False       # pole jest mina
-    __mine_neighbours = 0   # ile min na sąsiednich polach
-    __is_mine_chance = 1.0  # szansa na obecność miny na tym polu
-    __x = 0                 # (x,y) - lokacja miny na planszy
-    __y = 0
-    __game_id = ""
-    __game_class = ""
-    __clicked = False
+    is_mine = False       # pole jest mina
+    mine_neighbours = 0   # ile min na sąsiednich polach
+    is_mine_chance = 1.0  # szansa na obecność miny na tym polu
+    x = 0                 # (x,y) - lokacja miny na planszy
+    y = 0
+    game_id = ""
+    game_class = ""
+    clicked = False
+    neighbours_solved = False
 
     def __init__(self, x, y):
-        self.__x = x
-        self.__y = y
-        self.__game_id = str(y + 1) + "_" + str(x + 1)
+        self.x = x
+        self.y = y
+        self.game_id = str(y + 1) + "_" + str(x + 1)
+        self.game_class = "square blank"
 
     def set_mine(self):
-        self.__is_mine = True
+        self.is_mine = True
 
     def set_game_class(self, game_class: ""):
-        self.__game_class = game_class
+        self.game_class = game_class
         if game_class != "square blank":
-            self.__clicked = True
+            self.clicked = True
 
-            self.__mine_neighbours = {
+            self.mine_neighbours = {
                 'square bombrevealed' : 'M',
                 'square bombflagged': 'F',
                 'square open0': 0,
@@ -35,19 +37,13 @@ class Field:
                 'square open8': 8,
             }[game_class]
 
-            if self.__mine_neighbours == 'F':
+            if self.mine_neighbours == 'F':
                 self.set_mine()
 
-    def get_game_class(self):
-        return self.__game_class
-
-    def get_game_id(self):
-        return self.__game_id
-
     def __str__(self):
-        if self.__is_mine:
+        if self.is_mine:
             return "M"
-        elif self.__clicked is False and self.__mine_neighbours == 0:
+        elif self.clicked is False and self.mine_neighbours == 0:
             return "-"
         else:
-            return str(self.__mine_neighbours)
+            return str(self.mine_neighbours)
--- main.py
+++ main.py
@@ -3,31 +3,33 @@ import time
 from selenium import webdriver
 from Board import Board
 
-def send_some_clicks(board):
-    for i in range(29):
-        if board.send_click(0, i) == False:
-            board.print()
-            return False
-        board.print()
-    return True
+
 
 if __name__ == "__main__":
     driver = webdriver.Firefox()
     driver.get("http://minesweeperonline.com")
+    # driver.get("http://minesweeperonline.com/#beginner")
     assert "Minesweeper Online" in driver.title
 
     game = driver.find_element_by_id("game")
     height = int(len(game.find_elements_by_class_name('borderlr')) / 2)
     width = int(len(game.find_elements_by_class_name('bordertb')) / 3)
 
-    board = Board(game, height, width)
+    mines_counter = int(game.find_element_by_id("mines_hundreds").get_attribute("class")[-1])*100
+    mines_counter += int(game.find_element_by_id("mines_tens").get_attribute("class")[-1])*10
+    mines_counter += int(game.find_element_by_id("mines_ones").get_attribute("class")[-1])
+
+
+    board = Board(driver, game, height, width, mines_counter)
     board.print()
     print()
 
-    if send_some_clicks(board):
+    if board.play():
         print("YOU WON!")
     else:
         print("YOU LOST")
+
+    board.print()
 
     time.sleep(10.0)
     driver.close()
