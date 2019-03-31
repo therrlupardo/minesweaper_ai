917b16a9e38a7aec96f70302c54e8defa79d1844
Mateusz Buchajewicz <theherrlupardo@gmail.com>
2019-03-31 14:36:14 +0200

    wip

--- Field.py
+++ Field.py
@@ -1,7 +1,7 @@
 class Field:
     is_mine = False       # pole jest mina
-    mine_neighbours = 0   # ile min na sąsiednich polach
-    is_mine_chance = 1.0  # szansa na obecność miny na tym polu
+    mine_neighbours:int = 0   # ile min na sąsiednich polach
+    is_mine_chance = 0.0  # szansa na obecność miny na tym polu
     x = 0                 # (x,y) - lokacja miny na planszy
     y = 0
     game_id = ""
